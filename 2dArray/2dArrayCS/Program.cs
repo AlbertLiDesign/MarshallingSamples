886bbd67451c7f4abcd2a268531692199baf47e2
Albert Li <albertlidesign@gmail.com>
2020-05-25 21:26:25 +0800

    updated 2d array

--- 2dArray/2dArrayCS/Program.cs
+++ 2dArray/2dArrayCS/Program.cs
@@ -10,24 +10,34 @@ namespace _2dArrayCS
     class Program
     {
         [DllImport("2dArrayCPP.dll", EntryPoint = "Sum")]
-        public static extern void Sum(int[,] a, int b, int m,int n);
+        public static extern unsafe void Sum(int*[] a, int b, int m,int n);
         static void Main(string[] args)
         {
             int m = 3;
             int n = 5;
             var array = new int[3, 5] { { 0, 1, 2, 3, 4 }, { 5, 6, 7, 8, 9 }, { 10, 11, 12, 13, 14 } };
 
-            // remember to check "Allow unsafe code".
-            Sum(array, 4, m,n);
-
-            for (int i = 0; i < m; i++)
+            unsafe
             {
-                for (int j = 0; j < n; j++)
+                fixed (int* fp = array)
                 {
-                    Console.WriteLine(array[i,j]);
+                    int*[] farray = new int*[m];
+                    for (int i = 0; i < m; i++)
+                    {
+                        farray[i] = fp + i * n;
+                    }
+                    // remember to check "Allow unsafe code".
+                    Sum(farray, 4, m, n);
+                    for (int i = 0; i < m; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            Console.WriteLine(farray[i][j]);
+                        }
+                    }
+                    Console.ReadKey();
                 }
             }
-            Console.ReadKey();
         }
     }
 }
